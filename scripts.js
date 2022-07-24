e9a2b83a9cca85c9c88f6f7c639736bbb283d729
Ashish <ashishsark98@gmail.com>
2022-07-24 19:01:43 +0530

    Complete modal part with all design

--- scripts.js
+++ scripts.js
@@ -3,3 +3,23 @@ $(".bookmarkbtn").click(function(){
     $(".bookmark-img").attr("src")!=="images/icon-bookmark-selected.svg"?"images/icon-bookmark-selected.svg":"images/icon-bookmark.svg");
     $(".bookmarkbtn").css("color",$(".bookmarkbtn").css("color")!="rgb(20, 123, 116)"?"rgb(20, 123, 116)": "gray");
 });
+
+$(".sub").click(function(){
+    let par=$(this).parent().parent().parent();
+    let chi=par.children("div.radio-col").children("label.radio-container");
+    chi.click();
+    loadSubmitModal($(this));
+});
+
+$(".radio-col").click(function(){
+    let par=$(this).parent().parent();
+    $(".card").css("borderColor","#eff1f1");
+    $(".card").children("div.row.mt-0").addClass("hidden");
+    par.css("borderColor","hsl(176, 50%, 47%)");
+    par.children("div.row").removeClass("hidden");
+    loadSubmitModal($(this));
+});
+
+function loadSubmitModal(clickedele){
+
+}
