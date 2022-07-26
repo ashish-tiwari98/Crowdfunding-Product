9906107918626cdef5e7be843e3b861148f8307a
Ashish <ashishsark98@gmail.com>
2022-07-26 20:05:57 +0530

    mobile design main page

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-$(".bookmarkbtn").click(function(){
+$(".bookmarkbtn, .bookmark-img").click(function(){
     $(".bookmark-img").attr("src",
     $(".bookmark-img").attr("src")!=="images/icon-bookmark-selected.svg"?"images/icon-bookmark-selected.svg":"images/icon-bookmark.svg");
     $(".bookmarkbtn").css("color",$(".bookmarkbtn").css("color")!="rgb(20, 123, 116)"?"rgb(20, 123, 116)": "gray");
